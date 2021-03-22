343a921ca113bbdec65ccc8c756fdbb4fb9a9550
capricornus44 <antonzavalniuk@gmail.com>
2021-03-22 15:18:00 +0100

    create initial files

---
+++ src/js/modal.js
@@ -0,0 +1,4 @@
+// Import of modal plugin
+
+import * as basicLightbox from 'basiclightbox';
+import 'basicLightbox/dist/basicLightbox.min.css';
---
+++ src/js/servise/api.js
@@ -0,0 +1,4 @@
+const API_KEY = '989c90c59500ad26e3fa4e26d53d2bd3';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="?page=1`;
+const TREND_API = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+const MOVIE_API = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
