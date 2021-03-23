0e4adb8af3f91311f19065cb16badf87030098c5
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-23 17:10:56 +0200

    make styles for card

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
 // Import of modal plugin
 
-import * as basicLightbox from 'basiclightbox';
-import 'basicLightbox/dist/basicLightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
+// import 'basicLightbox/dist/basicLightbox.min.css';
 
-import modalCardTpl from '../templates/modalCard.hbs';
+// import modalCardTpl from '../templates/modalCard.hbs';
