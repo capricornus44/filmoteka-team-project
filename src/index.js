97d1190f09cd26643dd279b283ccd0ff45dda13d
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-03-23 15:11:14 +0200

    fixed modal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/main.scss';
 import './js/gallery';
+import './js/modal.js';
--- src/js/modal.js
+++ src/js/modal.js
@@ -2,3 +2,5 @@
 
 import * as basicLightbox from 'basiclightbox';
 import 'basicLightbox/dist/basicLightbox.min.css';
+
+import modalCardTpl from '../templates/modalCard.hbs';
