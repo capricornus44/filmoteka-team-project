51e354137389ed50e089849977936a1ac2174da4
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-03-24 18:23:10 +0200

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,7 +3,7 @@ import 'tui-pagination/dist/tui-pagination.css';
 import ApiService from './servise/api';
 import { printFilmography } from './gallery';
 
-const paginationRef = document.querySelector(`[data-pagination-value="pag"]`);
+// const paginationRef = document.querySelector(`[data-pagination-value="pag"]`);
 
 const apiService = new ApiService();
 const container = document.getElementById('pagination');
@@ -11,9 +11,25 @@ const pagination = new Pagination(container, {
   totalItems: 16000,
   //   totalPage: apiService.pageCount,
   itemsPerPage: 20,
-  visiblePages: 10,
+  visiblePages: 5,
   page: 1,
   centerAlign: true,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>'
+  }
 });
 
 pagination.on('beforeMove', async evt => {
